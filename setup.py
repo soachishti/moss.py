b380b8d3fd604a4a3a9c4627b09fd1f0c317e847
Owais <soachishti@outlook.com>
2017-09-13 16:35:35 +0500

    PyPI Upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'moss.py',
+  packages = ['moss'], # this must be the same as the name above
+  version = '1.0.0',
+  description = 'A Python client for moss: A System for Detecting Software Similarity',
+  author = 'soachishti',
+  author_email = 'soachishti@outlook.com',
+  url = 'https://github.com/soachishti/moss.py', # use the URL to the github repo
+  download_url = 'https://github.com/soachishti/moss.py/archive/v1.0.0.tar.gz', # I'll explain this in a second
+  keywords = ['moss', 'similarity', 'detecting', 'plagiarism'], # arbitrary keywords
+  classifiers = [],
+)
