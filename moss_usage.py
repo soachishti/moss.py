c14fb04a7e46f1b7675795ff43f70233f494ee28
Syed Owais Ali Chishti <soac@outlook.com>
2017-09-16 21:08:15 +0500

    Update moss_usage.py

--- moss_usage.py
+++ moss_usage.py
@@ -1,8 +1,8 @@
-import moss_py
+import mosspy
 
 userid = 987654321
 
-m = moss_py.Moss(userid, "python")
+m = mosspy.Moss(userid, "python")
 
 #m.addBaseFile("submission/a01.py")
 #m.addBaseFile("submission/test_student.py")
