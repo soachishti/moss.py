92986bca171b97fb9c47e40afb3ee2d8a08d1b41
Aaron Moss <mossa@up.edu>
2020-08-05 19:56:41 -0700

    Add documentation for lambda parameters

--- moss_usage.py
+++ moss_usage.py
@@ -12,11 +12,16 @@ m.addFile("submission/a01-sample.py")
 
 m.addFilesByWildcard("submission/a01-*.py")
 
-url = m.send() 
+# progress function optional, run on every file uploaded
+# result is submission URL
+url = m.send(lambda file_path, display_name: print('*', end='', flush=True))
+print()
 
 print ("Report URL: " + url)
 
 # Save report file
 m.saveWebPage(url, "submission/report.html")
 
-mosspy.download_report(url, "submission/report/", connections=8, log_level=10) # logging.DEBUG (20 to disable)
+mosspy.download_report(url, "submission/report/", connections=8, log_level=10, on_read=lambda url: print('*', end='', flush=True)) 
+# log_level=logging.DEBUG (20 to disable)
+# on_read function run for every downloaded file
